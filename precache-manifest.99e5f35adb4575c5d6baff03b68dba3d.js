fe3f3ed3d0af4627a1f806cc76f86c8185c22e0a
thanhmai <tranthithanhmai134@gmail.com>
2019-07-02 13:17:13 +0700

    Updates

---
+++ precache-manifest.99e5f35adb4575c5d6baff03b68dba3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800e6663d50be4cf1ae6b009694fddfc",
+    "url": "/shop-titan-react-redux/index.html"
+  },
+  {
+    "revision": "27ab0ea0b8ee54e51881",
+    "url": "/shop-titan-react-redux/static/css/main.89ecc2b1.chunk.css"
+  },
+  {
+    "revision": "b164d7d79cfdcf9049c8",
+    "url": "/shop-titan-react-redux/static/js/2.2976057b.chunk.js"
+  },
+  {
+    "revision": "27ab0ea0b8ee54e51881",
+    "url": "/shop-titan-react-redux/static/js/main.437a5a10.chunk.js"
+  },
+  {
+    "revision": "582242819c6430a83d50",
+    "url": "/shop-titan-react-redux/static/js/runtime~main.4e1096fa.js"
+  }
+]);
